5c344abcabad33dc30e4240f2853f8bf94d51155
wangshuz <17274578@qq.com>
2024-08-29 21:50:18 +0800

    加了注释

--- sjsd-project/src/App.vue
+++ sjsd-project/src/App.vue
@@ -5,6 +5,8 @@
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
+    <!-- 当用户点击导航链接（例如 <router-link to="/">Home</router-link>），URL 会改变，Vue Router 会根据当前 URL 匹配到相应的路由配置。
+匹配到的组件（例如 HomeView 或 AboutView）会被渲染到 <router-view/> 位置。 -->
   </div>
 </template>
 
