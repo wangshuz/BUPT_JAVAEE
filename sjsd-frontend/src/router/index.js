220dacb2077f151d9ac7c869832519d5cfacb7ea
wangshuz <17274578@qq.com>
2024-08-30 14:06:47 +0800

    补全了router里面的用户页面的所有路径

--- sjsd-frontend/src/router/index.js
+++ sjsd-frontend/src/router/index.js
@@ -1,20 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import( '../views/AboutView.vue')
-  },
   {
     path: '/mch',
     name: 'MchHome',
@@ -39,6 +28,31 @@ const routes = [
     path: '/mch/data',
     name: 'MchData',
     component: () => import( '../views/MCH/MchData.vue')
+  },
+  {
+    path: '/clt',
+    name: 'CltHome',
+    component: () => import( '../views/CLT/CltHome.vue')
+  },
+  {
+    path: '/clt/order',
+    name: 'CltOrder',
+    component: () => import( '../views/CLT/CltOrder.vue')
+  },
+  {
+    path: '/clt/payment',
+    name: 'CltPayment',
+    component: () => import( '../views/CLT/CltPayment.vue')
+  },
+  {
+    path: '/clt/store',
+    name: 'CltStore',
+    component: () => import( '../views/CLT/CltStore.vue')
+  },
+  {
+    path: '/clt/order/detail',
+    name: 'CltOrderDetail',
+    component: () => import( '../views/CLT/CltOrderDetail.vue')
   },
 ]
 
