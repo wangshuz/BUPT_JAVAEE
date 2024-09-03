f9739f44ea5790992229a4d8f305d584f14873f8
Xieyx <2022211464@bupt.cn>
2024-09-03 19:06:52 +0800

    后端工程创建

---
+++ sjsd-backend/src/test/java/buptworker/SjsdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package buptworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjsdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
