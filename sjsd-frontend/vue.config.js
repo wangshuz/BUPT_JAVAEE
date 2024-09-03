2180a598c9cac96e6bf86ebcb19c33813509fbc4
Xieyx <2022211464@bupt.cn>
2024-09-03 19:16:50 +0800

    vue.config.js 恢复

--- sjsd-frontend/vue.config.js
+++ sjsd-frontend/vue.config.js
@@ -1,18 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-        '/api': {
-            // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
-            target: 'http://localhost:8080/',
-            // 允许跨域
-            changeOrigin: true,
-            ws: true,
-            pathRewrite: {
-                '^/api': ''
-            }
-        }
-    }
-}
+  // devServer: {
+  //   proxy: {
+  //       '/api': {
+  //           // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
+  //           target: 'http://localhost:8080/',
+  //           // 允许跨域
+  //           changeOrigin: true,
+  //           ws: true,
+  //           pathRewrite: {
+  //               '^/api': ''
+  //           }
+  //       }
+  //   }
+  // } 
 })
