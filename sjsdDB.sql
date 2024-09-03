088ad1d9459965c5554e1566325ee99582e9547b
Xieyx <2022211464@bupt.cn>
2024-09-03 21:43:22 +0800

    数据库修改

---
+++ sjsdDB.sql
@@ -0,0 +1,196 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MySQL
+Source Server Version : 80034
+Source Host           : localhost:3306
+Source Database       : sjsd
+
+Target Server Type    : MYSQL
+Target Server Version : 80034
+File Encoding         : 65001
+
+Date: 2024-09-03 21:37:16
+*/
+
+CREATE DATABASE sjsd;
+USE sjsd;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `address`
+-- ----------------------------
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address` (
+  `address_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `recipient_name` varchar(255) NOT NULL,
+  `recipient_phone` varchar(20) NOT NULL,
+  `address_line` varchar(255) NOT NULL,
+  `is_default` tinyint(1) DEFAULT '0',
+  `is_deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`address_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of address
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `merchant`
+-- ----------------------------
+DROP TABLE IF EXISTS `merchant`;
+CREATE TABLE `merchant` (
+  `merchant_id` int NOT NULL AUTO_INCREMENT,
+  `merchant_username` varchar(255) NOT NULL,
+  `merchant_password` varchar(255) NOT NULL,
+  `merchant_name` varchar(255) NOT NULL,
+  `merchant_avatar` varchar(255) DEFAULT NULL,
+  `merchant_address` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `opening_hours` varchar(255) DEFAULT NULL,
+  `type_id` int DEFAULT NULL,
+  `merchant_description` text,
+  `is_open` tinyint(1) DEFAULT '1',
+  `delivery_fee` decimal(10,2) NOT NULL,
+  `minimum_order_amount` decimal(10,2) NOT NULL,
+  `packaging_fee_per_item` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`merchant_id`),
+  UNIQUE KEY `merchant_username` (`merchant_username`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `merchant_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `merchant_type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of merchant
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `merchant_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `merchant_type`;
+CREATE TABLE `merchant_type` (
+  `type_id` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  UNIQUE KEY `type_name` (`type_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of merchant_type
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `merchant_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `address_id` int DEFAULT NULL,
+  `order_date` datetime NOT NULL,
+  `estimated_delivery_time` datetime DEFAULT NULL,
+  `actual_delivery_time` datetime DEFAULT NULL,
+  `payment_method` varchar(50) NOT NULL,
+  `packaging_fee` decimal(10,2) NOT NULL,
+  `delivery_fee` decimal(10,2) NOT NULL,
+  `order_status` varchar(50) NOT NULL,
+  `notes` text,
+  `is_deleted_by_user` tinyint(1) DEFAULT '0',
+  `is_deleted_by_merchant` tinyint(1) DEFAULT '0',
+  `total_amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `merchant_id` (`merchant_id`),
+  KEY `user_id` (`user_id`),
+  KEY `address_id` (`address_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `order_item`
+-- ----------------------------
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE `order_item` (
+  `order_item_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `order_id` (`order_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of order_item
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `product`
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `merchant_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `description` text,
+  `available` tinyint(1) DEFAULT '1',
+  `is_deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`product_id`),
+  KEY `merchant_id` (`merchant_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`),
+  CONSTRAINT `product_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `product_category`
+-- ----------------------------
+DROP TABLE IF EXISTS `product_category`;
+CREATE TABLE `product_category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `merchant_id` int DEFAULT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`category_id`),
+  KEY `merchant_id` (`merchant_id`),
+  CONSTRAINT `product_category_ibfk_1` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`merchant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of product_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
