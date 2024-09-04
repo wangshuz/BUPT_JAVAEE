aa51ad872e0fbada6e1cc44c63bd06ece4d7fed3
Xieyx <2022211464@bupt.cn>
2024-09-04 12:54:06 +0800

    backend 上传

---
+++ sjsd-backend/src/main/java/buptworker/SjsdBackendApplication.java
@@ -0,0 +1,13 @@
+package buptworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjsdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjsdBackendApplication.class, args);
+    }
+
+}
---
+++ sjsd-backend/src/main/java/buptworker/service/impl/CltMainService_.java
@@ -0,0 +1,38 @@
+package buptworker.service.impl;
+
+import buptworker.mapper.CltMainMapper;
+import buptworker.pojo.MchIntro;
+import buptworker.pojo.MchType;
+import buptworker.service.CltMainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author : [Xieyx]
+ * @version : [v1.0]
+ * @description : [一句话描述该类的功能]
+ * @createTime : [2024/9/3 21:00]
+ * @updateUser : [86183]
+ * @updateTime : [2024/9/3 21:00]
+ * @updateRemark : [说明本次修改内容]
+ */
+
+@Component
+public class CltMainService_ implements CltMainService {
+
+    @Autowired
+    private CltMainMapper cltMainMapper;
+
+    @Override
+    public List<MchIntro> listMchIntro() {
+        return cltMainMapper.listMchIntro();
+    }
+
+    @Override
+    public List<MchType> listMchType() {
+        return cltMainMapper.listMchType();
+    }
+
+}
