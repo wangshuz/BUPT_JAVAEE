a1c6db4d2b9f73602bafc124a5ec57a5d7b1e6f9
Xieyx <2022211464@bupt.cn>
2024-09-06 02:27:04 +0800

    登录注册后端完成

--- sjsd-backend/src/main/java/buptworker/mapper/AuthMapper.java
+++ sjsd-backend/src/main/java/buptworker/mapper/AuthMapper.java
@@ -14,7 +14,7 @@ public interface AuthMapper {
     User findByCltname(@Param("username") String username);
 
     @Select("SELECT merchant_id AS userId, merchant_username AS username, merchant_password AS password " +
-            "FROM Merchant WHERE username = #{username}")
+            "FROM Merchant WHERE merchant_username = #{username}")
     User findByMchname(@Param("username") String username);
 
     // 检查用户名是否已存在
@@ -26,10 +26,10 @@ public interface AuthMapper {
     void insertClt(User user);
 
 
-    @Select("SELECT COUNT(*) FROM User WHERE username = #{username}")
+    @Select("SELECT COUNT(*) FROM Merchant WHERE merchant_username = #{username}")
     int countByMchname(@Param("username") String username);
 
 
-    @Insert("INSERT INTO User (username, password) VALUES (#{username}, #{password})")
+    @Insert("INSERT INTO Merchant (merchant_username, merchant_password) VALUES (#{username}, #{password})")
     void insertMch(User user);
 }
