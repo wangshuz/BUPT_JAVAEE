b034771ee02e774e819fef1c522d08bb6a1da63c
Lww0611 <161685777+Lww0611@users.noreply.github.com>
2024-09-05 21:15:53 +0800

    Update MchDetailMapper.java

--- sjsd-backend/src/main/java/buptworker/mapper/MchDetailMapper.java
+++ sjsd-backend/src/main/java/buptworker/mapper/MchDetailMapper.java
@@ -9,22 +9,29 @@ import java.util.List;
 @Mapper
 public interface MchDetailMapper {
 
-    @Select("SELECT * FROM merchant WHERE merchant_id = #{merchantID}")
+    @Select("SELECT m.merchant_id, m.merchant_avatar, m.merchant_name, m.type_id, " +
+            "t.type_name, m.delivery_fee, m.packaging_fee_per_item, m.minimum_order_amount, " +
+            "m.opening_hours, m.phone_number, m.merchant_address, m.merchant_description, m.is_open " +
+            "FROM merchant m " +
+            "JOIN merchant_type t ON m.type_id = t.type_id " +
+            "WHERE m.merchant_id = #{merchantID}")
     @Results({
-            @Result(property = "merchantID", column = "merchant_id"), // 数据库字段 merchant_id 对应类中的 merchantID
-            @Result(property = "avatarURL", column = "merchant_avatar"), // 数据库字段 avatar_url 对应类中的 avatarURL
-            @Result(property = "merchantName", column = "merchant_name"), // 数据库字段 merchant_name 对应类中的 merchantName
-            @Result(property = "typeName", column = "type_id"), // 数据库字段 type_name 对应类中的 typeName
-            @Result(property = "deliveryFee", column = "delivery_fee"), // 数据库字段 delivery_fee 对应类中的 deliveryFee
-            @Result(property = "packagingFeePerItem", column = "packaging_fee_per_item"), // 数据库字段 packaging_fee_per_item 对应类中的 packagingFeePerItem
-            @Result(property = "minimumOrderAmount", column = "minimum_order_amount"), // 数据库字段 minimum_order_amount 对应类中的 minimumOrderAmount
-            @Result(property = "openingHours", column = "opening_hours"), // 数据库字段 opening_hours 对应类中的 openingHours
-            @Result(property = "phoneNumber", column = "phone_number"), // 数据库字段 phone_number 对应类中的 phoneNumber
-            @Result(property = "merchantAddress", column = "merchant_address"), // 数据库字段 merchant_address 对应类中的 merchantAddress
-            @Result(property = "merchantDescription", column = "merchant_description"), // 数据库字段 merchant_description 对应类中的 merchantDescription
-            @Result(property = "isOpen", column = "is_open") // 数据库字段 is_open 对应类中的 isOpen
+            @Result(property = "merchantID", column = "merchant_id"),
+            @Result(property = "avatarURL", column = "merchant_avatar"),
+            @Result(property = "merchantName", column = "merchant_name"),
+            @Result(property = "typeId", column = "type_id"),
+            @Result(property = "typeName", column = "type_name"), // 映射 typeName
+            @Result(property = "deliveryFee", column = "delivery_fee"),
+            @Result(property = "packagingFeePerItem", column = "packaging_fee_per_item"),
+            @Result(property = "minimumOrderAmount", column = "minimum_order_amount"),
+            @Result(property = "openingHours", column = "opening_hours"),
+            @Result(property = "phoneNumber", column = "phone_number"),
+            @Result(property = "merchantAddress", column = "merchant_address"),
+            @Result(property = "merchantDescription", column = "merchant_description"),
+            @Result(property = "isOpen", column = "is_open")
     })
     Merchant getMerchantById(int merchantID);
+
 
     @Update("UPDATE merchant SET merchant_name = #{merchantName}, merchant_address = #{merchantAddress}, " +
             "phone_number = #{phoneNumber}, opening_hours = #{openingHours}, " +
