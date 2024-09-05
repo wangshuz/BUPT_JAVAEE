0abc86263fcb700b967f112c44d8af497caab99f
Xieyx <2022211464@bupt.cn>
2024-09-06 01:35:50 +0800

    首页修改

--- sjsd-frontend/src/views/HomePage.vue
+++ sjsd-frontend/src/views/HomePage.vue
@@ -8,7 +8,6 @@
           <div class="stitle">商家入驻</div>
           <div class="sintro">加入舌尖速递，开启您的在线商业之旅。我们提供全面的技术支持和营销策略，帮助您的餐厅吸引更多顾客，提高品牌知名度。</div>
           <div class="sintro">通过我们的平台，您可以轻松管理订单，优化菜单，并获取深入的数据分析，以提升您的业务效率。</div>
-          <button class="lbutton" @click="onLeftButtonClick">加入我们</button>
         </div>
       </div>
   
@@ -18,7 +17,6 @@
           <div class="stitle">舌尖速递</div>
           <div class="sintro">我们致力于提供最优质的外卖服务，让每一位顾客都能享受到便捷的订餐体验和美味的佳肴。</div>
           <div class="sintro">无论是家庭聚餐还是办公室午餐，舌尖速递都能满足您的需求。我们承诺快速配送，确保每一份食物都新鲜可口。</div>
-          <button class="rbutton" @click="onRightButtonClick">立即体验</button>
         </div>
       </div>
   
@@ -29,9 +27,12 @@
         <div id="line2">在线订餐，轻松快捷</div>
         <div id="line3">发现周边美食，享受生活每一刻</div>
         <div id="line4">立即体验舌尖速递，让美味触手可及</div>
+        <button class="center-button" @click="onCentralButtonClick">立即体验</button>
       </div>
     </div>
   </template>
+  
+
 
 <script>
 export default {
@@ -61,14 +62,7 @@ export default {
         this.isBackShown = this.isLeftOpen;
       }
     },
-    onLeftButtonClick() {
-      this.$router.push(
-            {
-                path:"/login"
-            }
-        )
-    },
-    onRightButtonClick() {
+    onCentralButtonClick() {
         this.$router.push(
             {
                 path:"/login"
@@ -212,42 +206,9 @@ export default {
     font-size: 40px;
     line-height: 60px;
   }
-  .center {
-    background-color: rgba(168, 164, 164, 0.479);
-    width: 900px;
-    height: 400px;
-    border-radius: 20px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    z-index: 5;
-  }
-  #title {
-    font-size: 90px;
-    font-weight: 500;
-  }
-  #line1 {
-    font-size: 48px;
-    line-height: 90px;
-  }
-  #line2 {
-    font-size: 21px;
-    line-height: 60px;
-  }
-  #line3 {
-    font-size: 14px;
-    line-height: 32px;
-  }
-  .ToolBar {
-    background-color: rgba(155, 155, 155, 0.8);
-    width: 100%;
-    height: 75px;
-    position: absolute;
-    bottom: 0;
-    z-index: 100;
-  }
+
+  
+
   .back {
     width: 100%;
     height: 100%;
@@ -261,21 +222,61 @@ export default {
     z-index: 15;
   }
 
-  .lbutton,
-.rbutton {
-  display: inline-block;
-  margin-top: 20px;
-  padding: 10px 20px;
-  font-size: 16px;
-  color: white;
+  .container {
+  width: 100%;
+  height: 100vh; /* 使用视口高度 */
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.center {
+  background-color: rgba(168, 164, 164, 0.479);
+  width: 900px;
+  height: 500px; /* 增加高度 */
+  border-radius: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  z-index: 5;
+}
+
+.center-button {
+  margin-top: 40px;
+  padding: 15px 30px;
+  font-size: 18px;
   background-color: #ff5722;
+  color: white;
   border: none;
   border-radius: 5px;
   cursor: pointer;
 }
 
-.lbutton:hover,
-.rbutton:hover {
+.center-button:hover {
   background-color: #e64a19;
+}
+
+#title {
+  font-size: 90px;
+  font-weight: 500;
+}
+
+#line1 {
+  font-size: 48px;
+  line-height: 90px;
+}
+
+#line2 {
+  font-size: 21px;
+  line-height: 60px;
+}
+
+#line3 {
+  font-size: 14px;
+  line-height: 32px;
 }
   </style>
