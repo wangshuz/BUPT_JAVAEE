74b0f1ff5dd68fb381e0a00ee9c98eb7adcf2c9d
Lww0611 <161685777+Lww0611@users.noreply.github.com>
2024-09-05 18:15:37 +0800

    Update MchDetailMapper.java

--- sjsd-backend/src/main/java/buptworker/mapper/MchDetailMapper.java
+++ sjsd-backend/src/main/java/buptworker/mapper/MchDetailMapper.java
@@ -27,7 +27,7 @@ public interface MchDetailMapper {
     Merchant getMerchantById(int merchantID);
 
     @Update("UPDATE merchant SET merchant_name = #{merchantName}, merchant_address = #{merchantAddress}, " +
-            "phone_number = #{phoneNumber}, opening_hours = #{openingHours}, business_type = #{typeName}, " +
+            "phone_number = #{phoneNumber}, opening_hours = #{openingHours}, " +
             "merchant_description = #{merchantDescription}, is_open = #{isOpen}, delivery_fee = #{deliveryFee}, " +
             "minimum_order_amount = #{minimumOrderAmount}, packaging_fee_per_item = #{packagingFeePerItem} " +
             "WHERE merchant_id = #{merchantID}")
