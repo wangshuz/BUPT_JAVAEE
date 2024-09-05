eab89afab1f57f83e140722eb873324a38bd10fc
Lww0611 <161685777+Lww0611@users.noreply.github.com>
2024-09-05 21:15:58 +0800

    Update Merchant.java

--- sjsd-backend/src/main/java/buptworker/entity/Merchant.java
+++ sjsd-backend/src/main/java/buptworker/entity/Merchant.java
@@ -16,6 +16,7 @@ public class Merchant {
     private String avatarURL;
     private String merchantName;
     private int typeId;
+    private String typeName;
     private double deliveryFee;
     private double packagingFeePerItem;
     private double minimumOrderAmount;
@@ -28,7 +29,7 @@ public class Merchant {
     public Merchant() {
     }
 
-    public Merchant(int merchantID, String avatarURL, String merchantName, int typeId, double deliveryFee, double packagingFeePerItem, double minimumOrderAmount, String openingHours, String phoneNumber, String merchantAddress, String merchantDescription) {
+    public Merchant(int merchantID, String avatarURL, String merchantName, int typeId,String typeName, double deliveryFee, double packagingFeePerItem, double minimumOrderAmount, String openingHours, String phoneNumber, String merchantAddress, String merchantDescription) {
         this.merchantID = merchantID;
         this.merchantName = merchantName;
         this.avatarURL = avatarURL;
@@ -40,6 +41,7 @@ public class Merchant {
         this.minimumOrderAmount = minimumOrderAmount;
         this.packagingFeePerItem = packagingFeePerItem;
         this.typeId = typeId;
+        this.typeName = typeName;
 
     }
     public boolean getIsOpen() {
@@ -74,13 +76,13 @@ public class Merchant {
         this.merchantName = merchantName;
     }
 
-    /*public String getTypeName() {
+    public String getTypeName() {
         return typeName;
     }
 
     public void setTypeName(String typeName) {
         this.typeName = typeName;
-    }*/
+    }
 
     public int getTypeId() {
         return typeId;
