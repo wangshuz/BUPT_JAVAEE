bf43c7d4a78848f647c5b58c4ecb60e33dd803b8
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2024-09-06 01:25:10 +0800

    1

--- sjsd-frontend/src/api/loginAPI.js
+++ sjsd-frontend/src/api/loginAPI.js
@@ -19,25 +19,25 @@ const apiClient = axios.create({
 
 export default {
   // 商家登录
-  // 返回code: 1:登录成功 2:密码错误或用户不存在
+  // 返回code: 1:登录成功 0:密码错误或用户不存在
   async loginMerchant(username, password) {
     return await apiClient.post('/api/mch/login', { username, password });
   },
 
   // 商家注册
-  // 返回code: 1:注册成功 2:用户名已存在
+  // 返回code: 1:注册成功 0:用户名已存在
   async registerMerchant(username, password, type) {
     return await apiClient.post('/api/mch/register', { username, password});
   },
 
   // 用户登录
-  // 返回code: 1:登录成功 2:密码错误或用户不存在
+  // 返回code: 1:登录成功 0:密码错误或用户不存在
   async loginCustomer(username, password) {
     return await apiClient.post('/api/clt/login', { username, password });
   },
 
   // 用户注册
-  // 返回code: 1:注册成功 2:用户名已存在
+  // 返回code: 1:注册成功 0:用户名已存在
   async registerCustomer(username, password) {
     return await apiClient.post('/api/clt/register', { username, password });
   },
