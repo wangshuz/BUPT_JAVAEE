59f86a1d6fa468cf29736267d840c04f06774154
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2024-09-11 22:14:36 +0800

    Update vue.config.js

--- sjsd-frontend/vue.config.js
+++ sjsd-frontend/vue.config.js
@@ -1,18 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // devServer: {
-  //   proxy: {
-  //       '/api': {
-  //           // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
-  //           target: 'http://localhost:8080/',
-  //           // 允许跨域
-  //           changeOrigin: true,
-  //           ws: true,
-  //           pathRewrite: {
-  //               '^/api': ''
-  //           }
-  //       }
-  //   }
-  // } 
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist', // 打包输出文件夹
+};
